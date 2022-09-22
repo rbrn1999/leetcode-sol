89bea5a23ba59c576978ca1818fe9ccab8e36b5b
Ian Liu <ianliu@livemail.tw>
2022-09-22 14:15:45 +0800

    add problem 342.

---
+++ problems/342. Power of Four.py
@@ -0,0 +1,5 @@
+# link: https://leetcode.com/problems/power-of-four/
+# solution: https://leetcode.com/problems/power-of-four/discuss/80461/Python-one-line-solution-with-explanations
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n != 0 and n&(n-1) == 0 and n&1431655765 == n
