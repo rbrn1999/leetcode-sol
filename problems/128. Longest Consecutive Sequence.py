e0668bd18959332efb3528aad6a2ee0acbe9a170
Ian Liu <ianliu@livemail.tw>
2022-10-20 23:16:21 +0800

    add a solution to problem 128.

--- problems/128. Longest Consecutive Sequence.py
+++ problems/128. Longest Consecutive Sequence.py
@@ -26,3 +26,20 @@ class Solution:
                 s.add(num)
 
         return longestStreak
+
+# alternate solution
+# reference: https://youtu.be/P6RZZMu_maU
+
+#class Solution:
+#    def longestConsecutive(self, nums: List[int]) -> int:
+#        longest = 0
+#        s = set(nums)
+#
+#        for num in nums:
+#            if (num-1) not in s:
+#                length = 1
+#                while num+length in s:
+#                    length += 1
+#                longest = max(length, longest)
+#
+#        return longest
