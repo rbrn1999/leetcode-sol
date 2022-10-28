2bc31591e4173f0b55e854a133e37872081b435f
Ian Liu <ianliu@livemail.tw>
2022-10-28 22:24:56 +0800

    add space complexity O(1) solution to problem 91.

--- problems/91. Decode Ways.py
+++ problems/91. Decode Ways.py
@@ -1,18 +1,42 @@
 # link: https://leetcode.com/problems/decode-ways/
-# solution reference: https://leetcode.com/problems/decode-ways/discuss/30358/
 
+
+
+'''
+Time Complexity: O(n)
+Space Complexity: O(1)
+'''
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        dp = len(s) * [0]
-        dp[0] = 1 if s[0] != '0' else 0
-        for i in range(1, len(s)):
-            if s[i] != '0':
-                dp[i] += dp[i-1]
+    def numDecodings(self, s: str) -> int: # need to review how does this avoid invalid cases (return 0)
+        if s[0] == '0':
+            return 0
+        prev = [1, 1]
+        for i in range(1, len(s)): 
+            cur = prev[1] if s[i] != '0' else 0
             if 10 <= int(s[i-1:i+1]) <= 26:
-                if i < 2:
-                    dp[i] += 1
-                else:
-                    dp[i] += dp[i-2]
+                cur += prev[0]
+            prev = [prev[1], cur]
+        
+        return prev[1]
 
-        return dp[-1]
+
+'''
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''
+# solution reference: https://leetcode.com/problems/decode-ways/discuss/30358/
+# class Solution:
+#     def numDecodings(self, s: str) -> int:
+#         dp = len(s) * [0]
+#         dp[0] = 1 if s[0] != '0' else 0
+#         for i in range(1, len(s)):
+#             if s[i] != '0':
+#                 dp[i] += dp[i-1]
+#             if 10 <= int(s[i-1:i+1]) <= 26:
+#                 if i < 2:
+#                     dp[i] += 1
+#                 else:
+#                     dp[i] += dp[i-2]
+
+#         return dp[-1]
 
