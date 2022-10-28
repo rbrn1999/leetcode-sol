081268f262e503921b9b4c443056844b8f3993f0
Ian Liu <ianliu@livemail.tw>
2022-10-28 21:49:45 +0800

    update to a cleaner solution for problem 647.

--- problems/647. Palindromic Substrings.py
+++ problems/647. Palindromic Substrings.py
@@ -1,18 +1,17 @@
 # link: https://leetcode.com/problems/palindromic-substrings/
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        def helper(start, end): # calculate Palindromic substrings expanded from a given center 
-            count = 0
-            while start >= 0 and end < len(s) and s[start] == s[end]:
-                count += 1
-                start -= 1
-                end += 1
-            return count
-            
+        res = 0
+        n = len(s)
+        def helper(l, r): # expand from the middle of palindromic string
+            nonlocal res
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+                res += 1
         
-        # count = 0
-        # for i in range(len(s)):
-        #     count += helper(i, i)
-        #     count += helper(i, i+1)
-        # return count
-        return sum(helper(i, i) + helper(i, i+1) for i in range(len(s)))
+        for i in range(n):
+            helper(i, i)
+            helper(i, i+1)
+            
+        return res
