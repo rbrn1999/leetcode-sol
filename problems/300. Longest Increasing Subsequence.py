29344aa0aa494a8265543764c6181281ddd2c55b
Ian Liu <ianliu@livemail.tw>
2022-10-29 17:32:27 +0800

    refactor and add details

--- problems/300. Longest Increasing Subsequence.py
+++ problems/300. Longest Increasing Subsequence.py
@@ -1,20 +1,31 @@
 # link: https://leetcode.com/problems/longest-increasing-subsequence/
-# solution reference: https://leetcode.com/problems/longest-increasing-subsequence/discuss/1326308/C%2B%2BPython-DP-Binary-Search-BIT-Solutions-Picture-explain-O(NlogN)
 
+'''
+Dynamic Programming
+Time Complexity: O(nlogn)
+Space Complexity: O(n)
+'''
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        memo = [1] * len(nums)
+        for i, num in enumerate(nums):
+            curLen = 1
+            for j in range(i):
+                if num > nums[j]:
+                    curLen = max(curLen, memo[j] + 1)
+            memo[i] = curLen
+        
+        return max(memo)
+
+'''
+Greedy with Binary Search
+Time Complexity: O(nlogn)
+Space Complexity: O(n)
+solution reference: https://leetcode.com/problems/longest-increasing-subsequence/discuss/1326308
+'''
 from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
-#         dp
-#         n = len(nums)
-#         dp = [1] * n
-#         for i in range(n):
-#             for j in range(i):
-#                 if nums[i] > nums[j]:
-#                     dp[i] = max(dp[i], dp[j]+1)
-        
-#         return max(dp)
-
-#       Greedy with Binary Search
         sub = []
         for num in nums:
             if not sub or sub[-1] < num:
