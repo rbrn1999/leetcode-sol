500bba7dae08d82fd6956ca48e890d8426fc9da7
Ian Liu <ianliu@livemail.tw>
2022-10-29 12:25:25 +0800

    add different approach for problem 322.

--- problems/322. Coin Change.py
+++ problems/322. Coin Change.py
@@ -1,5 +1,14 @@
 # link: https://leetcode.com/problems/coin-change/
 
+# class Solution:
+#     def coinChange(self, coins: list[int], amount: int) -> int:
+#         minCoins = [float('inf')] * (amount+1)
+#         minCoins[0] = 0 # base case: 0 coins to get to 0 total
+#         for i in range(1, amount+1):
+#             for coin in coins:
+#                 if coin <= amount:
+#                     minCoins[i] = min(minCoins[i], minCoins[i-coin]+1)
+#         return minCoins[amount] if minCoins[amount] != float('inf') else -1
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
         minCoins = [float('inf')] * (amount + 1)
