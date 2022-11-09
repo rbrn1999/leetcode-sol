4132768cb985cd6ad07d2a84f1600717839dad90
Ian Liu <ianliu@livemail.tw>
2022-11-10 00:38:18 +0800

    add BFS solution to problem 200.

--- problems/200. Number of Islands.py
+++ problems/200. Number of Islands.py
@@ -1,34 +1,53 @@
 # link: https://leetcode.com/problems/number-of-islands/
 
-from typing import List
+# DFS solution
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        def search(x, y):
-            if x<0 or x>=m or y<0 or y>=n:
-                return
-            if grid[x][y] == '1':
-                grid[x][y] = '0'
-                search(x-1, y)
-                search(x+1, y)
-                search(x, y-1)
-                search(x, y+1)
-                
-        m = len(grid)
-        n = len(grid[0])
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
-                    search(i, j)
+    def numIslands(self, grid: list[list[str]]) -> int:
+        m, n = len(grid), len(grid[0])
         
-        return count
+        def sink_island(row, col):
+            if row < 0 or col < 0 or row >= m or col >= n or grid[row][col] == '0':
+                return
+            grid[row][col] = '0'
+            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            
+            for d_row, d_col in directions:
+                sink_island(row+d_row, col+d_col)
+        
+        res = 0
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == '1':
+                    res += 1
+                    sink_island(row, col)
+        return res
 
-output = Solution().numIslands([
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-])
+# BFS solution
 
-print(output)
+from collections import deque
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        
+        def sink_island(row, col):
+            q = deque()
+            q.append((row, col))
+            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            while q:
+                row, col = q.popleft()
+                for off_r, off_c in directions:
+                    r = row + off_r
+                    c = col + off_c
+                    if r in range(m) and c in range(n) and grid[r][c] == '1':
+                        grid[r][c] = '0'
+                        q.append((r, c))
+        
+        res = 0
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == '1':
+                    grid[row][col] = '0'
+                    res += 1
+                    sink_island(row, col)
+        return res
+        
