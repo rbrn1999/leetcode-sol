503fadcd9da02442d5e0ba8b5c5fb325f26a860a
Ian Liu <ianliu@livemail.tw>
2022-11-16 16:18:13 +0800

    add iterative solution

--- problems/133. Clone Graph.py
+++ problems/133. Clone Graph.py
@@ -19,13 +19,33 @@ class Solution:
                     n_node = Node(n.val)
                     nodes[n.val] = n_node
                     dfs(n_node, n)
-
                 c_node.neighbors.append(nodes[n.val])
 
         root = Node(val=node.val)
         nodes = {root.val: root}
         dfs(root, node)
 
-
         return root
 
+# iterative solution
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node is None:
+            return None
+        
+        root = Node(val=node.val)
+        nodes = {root.val: root}
+        # dfs(root, node)
+        
+        stack = [(root, node)]
+        while stack:
+            curNode, ref = stack.pop()
+            for ref_n in ref.neighbors:
+                if ref_n.val not in nodes:
+                    nNode = Node(ref_n.val)
+                    nodes[ref_n.val] = nNode
+                    stack.append((nNode, ref_n))
+                curNode.neighbors.append(nodes[ref_n.val])
+        
+
+        return root
