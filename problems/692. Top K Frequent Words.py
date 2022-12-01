92819eac1966cfb96e5c9c2959042820dc9575cd
Ian Liu <ianliu@livemail.tw>
2022-12-01 12:18:54 +0800

    add heap solution

--- problems/692. Top K Frequent Words.py
+++ problems/692. Top K Frequent Words.py
@@ -18,4 +18,47 @@ class Solution:
         count_word[start:end] = sorted(count_word[start:end], key=lambda x: x[1])
         start = end
 
-        return [word for count, word in count_word[:k]]
+        return [word for _, word in count_word[:k]]
+
+
+'''
+heap solution
+Time Complexity: O(n*log(n))
+Space Complexity: O(n)
+'''
+import functools
+import heapq
+
+@functools.total_ordering
+class WordFreq:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+        
+    # treat less than as greater than to get heapq working as a max-heap
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word <= other.word
+        else:
+            return self.freq > other.freq
+    
+    def __eq__(self, other):
+        if self.freq == other.freq:
+            return self.word == other.word
+        else:
+            return False
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        result = []
+        counter = defaultdict(lambda: 0)
+        for word in words:
+            counter[word] += 1
+            
+        wordFreqs = [WordFreq(word, counter[word]) for word in counter]
+        heapq.heapify(wordFreqs)
+        
+        for _ in range(k):
+            result.append(heapq.heappop(wordFreqs).word)
+        
+        return result
