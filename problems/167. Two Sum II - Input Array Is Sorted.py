61acf94016820b6c46ac45d9b5e74a5af2fb9608
Ian Liu <ianliu@livemail.tw>
2022-12-22 21:39:04 +0800

    change to constant space solution

--- problems/167. Two Sum II - Input Array Is Sorted.py
+++ problems/167. Two Sum II - Input Array Is Sorted.py
@@ -2,9 +2,22 @@
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        valueIndex = {}
-        for ind, num in enumerate(numbers):
-            if target-num in valueIndex.keys():
-                return [valueIndex[target-num], ind+1]
-            else:
-                valueIndex[num] = ind+1
+        def search(target, left, right):
+            while left <= right:
+                mid = left + (right - left) // 2
+                if numbers[mid] < target:
+                    left = mid + 1
+                elif numbers[mid] > target:
+                    right = mid - 1
+                else:
+                    return mid
+            return -1
+
+        n = len(numbers)
+        for i in range(n):
+            j = search(target - numbers[i], i+1, n-1)
+            if j == -1:
+                continue
+            return [i+1 ,j+1]
+
+        return [-1, -1]
