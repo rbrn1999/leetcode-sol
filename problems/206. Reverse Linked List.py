477ed9888c8a14a73ed33f98fd39f8b1bb1f200e
Ian Liu <ianliu@livemail.tw>
2022-12-22 12:49:30 +0800

    add recursive solution

--- problems/206. Reverse Linked List.py
+++ problems/206. Reverse Linked List.py
@@ -1,3 +1,4 @@
+# link: https://leetcode.com/problems/reverse-linked-list/description/
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -9,6 +10,16 @@ class Solution:
         prev = None
         while cur:
             cur.next, cur, prev = prev, cur.next, cur
-        
+
         return prev
-            
+
+# recursive
+# solution: https://leetcode.com/problems/reverse-linked-list/solutions/58127/python-iterative-and-recursive-solution/comments/291532
+
+ def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        node = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return node
