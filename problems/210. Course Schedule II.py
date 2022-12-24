b0659d9ae14167e36878c3c769ac35741369cba0
Ian Liu <ianliu@livemail.tw>
2022-12-24 21:24:59 +0800

    add solution with better way to detect cycle

--- problems/210. Course Schedule II.py
+++ problems/210. Course Schedule II.py
@@ -1,36 +1,66 @@
-from collections import deque, defaultdict
-from collections import deque, defaultdict
+from collections import defaultdict
+
+# detect cycle while in process (much better approach)
+# reference: https://youtu.be/6kTZYvNNyps
+
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
         edges = defaultdict(list)
         for course, prerequisite in prerequisites:
             edges[course].append(prerequisite)
-        order = [-1] * numCourses
-        visited = [False] * numCourses
 
-        orderIndex = 0
+        order = []
+        visited = {} # False: visited, True: in path, not done(processed) yet
+
         def dfs(course):
+            if course in visited: # pass if visited and early return if cycle detected
+                return visited[course]
             visited[course] = True
-            for edge in edges[course]:
-                if visited[edge]:
-                    continue
-                dfs(edge)
 
-            nonlocal orderIndex
-            order[orderIndex] = course
-            orderIndex += 1
-            return True
+            for edge in edges[course]:
+                if dfs(edge):
+                    return True # cycle detected
+
+            order.append(course)
+            visited[course] = False
+            return False
 
         for course in range(numCourses):
-            if visited[course]:
-                continue
-            dfs(course)
-
-        # check cycle
-        positions = {course: pos for pos, course in enumerate(order)}
-        for i in range(numCourses):
-            for edge in edges[i]:
-                if positions[i] < positions[edge]:
-                    return []
+            if dfs(course):
+                return []
 
         return order
+
+
+# detect cycle afterhand
+# class Solution:
+#     def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+#         edges = defaultdict(list)
+#         for course, prerequisite in prerequisites:
+#             edges[course].append(prerequisite)
+#         order = []
+#         visited = [False] * numCourses
+
+#         def dfs(course):
+#             visited[course] = True
+#             for edge in edges[course]:
+#                 if visited[edge]:
+#                     continue
+#                 dfs(edge)
+
+#             order.append(course)
+#             return True
+
+#         for course in range(numCourses):
+#             if visited[course]:
+#                 continue
+#             dfs(course)
+
+#         # check cycle
+#         positions = {course: pos for pos, course in enumerate(order)}
+#         for i in range(numCourses):
+#             for edge in edges[i]:
+#                 if positions[i] < positions[edge]:
+#                     return []
+
+#         return order
