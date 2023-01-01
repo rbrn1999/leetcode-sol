25e0951a25eeab8cb02f9dee1dcb5331773425e0
Ian Liu <ianliu@livemail.tw>
2023-01-01 14:36:31 +0800

    add max heap and quick select solutions

--- problems/215. Kth Largest Element in an Array.py
+++ problems/215. Kth Largest Element in an Array.py
@@ -1,5 +1,32 @@
 # link: https://leetcode.com/problems/kth-largest-element-in-an-array/
-import heapq 
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # max heap: time complexity: O(n+klog(n))
+        maxHeap = [-num for num in nums]
+        heapq.heapify(maxHeap)
+        for _ in range(k-1):
+            heapq.heappop(maxHeap)
+        return -heapq.heappop(maxHeap)
+
+        # quick select: time complexity: average O(n), worst O(n^2)
+        # k -= 1 # convert to index
+        # def quickSelect(start=0, end=len(nums)-1):
+        #     pivot = nums[end]
+        #     p = start
+        #     for i in range(start, end):
+        #         if nums[i] >= pivot:
+        #             nums[i], nums[p] = nums[p], nums[i]
+        #             p += 1
+        #     nums[end], nums[p] = nums[p], nums[end]
+        #     if p == k:
+        #         return nums[p]
+        #     elif p > k:
+        #         return quickSelect(start, p-1)
+        #     else:
+        #         return quickSelect(p+1, end)
+
+        # return quickSelect()
 
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
