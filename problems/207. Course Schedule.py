12149542ee2620d26b9927474f63f4e3cb8f7ea9
Ian Liu <ianliu@livemail.tw>
2023-01-02 23:21:46 +0800

    change to more concise

--- problems/207. Course Schedule.py
+++ problems/207. Course Schedule.py
@@ -1,28 +1,25 @@
 from collections import defaultdict
+from functools import cache
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
         prereq_dict = defaultdict(list)
         for p in prerequisites:
             prereq_dict[p[0]].append(p[1])
-        finished = [False] * numCourses
-        def canFinish(course, seen):
-            if finished[course]:
-                return True
-            if course in seen:
-                return False
-            if course not in prereq_dict:
-                finished[course] = True
-                return True
-            seen.add(course)
+       
+        path = set()
+        visited = set()
+        @cache
+        def dfs(course):
+            path.add(course)
             for p in prereq_dict[course]:
-                if canFinish(p, seen):
-                    finished[p] = True
-                else:
-                    return False
-            seen.remove(course)
-            return True
-        
+                if p in visited:
+                    continue
+                if p in path or dfs(p):
+                    return True # cycle
+            path.remove(course)
+            return False
+
         for i in range(numCourses):
-            if not canFinish(i, set()):
+            if dfs(i):
                 return False
         return True
