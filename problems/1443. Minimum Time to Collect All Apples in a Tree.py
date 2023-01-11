eb37b809e4ff77582942f1061648b8966de26588
Ian Liu <ianliu@livemail.tw>
2023-01-11 09:27:07 +0800

    change to more efficient and concise solution

--- problems/1443. Minimum Time to Collect All Apples in a Tree.py
+++ problems/1443. Minimum Time to Collect All Apples in a Tree.py
@@ -2,21 +2,19 @@
 
 from collections import defaultdict
 class Solution:
-    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        nodeEdges = defaultdict(list)
-        for a, b in edges:
-            nodeEdges[a].append(b)
-            nodeEdges[b].append(a)
-        usedEdges = set()
-        def dfs(node, path):
-            path.append(node)
-            if hasApple[node]:
-                nonlocal usedEdges
-                for i in range(1, len(path)):
-                    usedEdges.add((path[i-1], path[i]))
-            for dest in nodeEdges[node]:
-                if dest not in path:
-                    dfs(dest, path.copy())
-
-        dfs(0, [])
-        return 2 * len(usedEdges)
+    def minTime(self, n: int, edges: list[list[int]], hasApple: list[bool]) -> int:
+        graph = defaultdict(set)
+        for source, dest in edges:
+            graph[source].add(dest)
+            graph[dest].add(source)
+        
+        def dfs(node: int, parent: int) -> int:
+            time = 0
+            for child in graph[node]:
+                if child == parent:
+                    continue
+                child_cost = dfs(child, node) 
+                time += child_cost + (2 if child_cost > 0 or hasApple[child] else 0)
+            return time
+        
+        return dfs(0, -1)
