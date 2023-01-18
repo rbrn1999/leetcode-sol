6a071886772579f5133cf836cb35ba4c93ed7042
Ian Liu <ianliu@livemail.tw>
2023-01-18 10:37:07 +0800

    add more optimal solution

--- problems/918. Maximum Sum Circular Subarray.py
+++ problems/918. Maximum Sum Circular Subarray.py
@@ -1,4 +1,5 @@
 # link: https://leetcode.com/problems/maximum-sum-circular-subarray/
+# Kadane's algorithm
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
@@ -19,3 +20,30 @@ class Solution:
         else:
             return max(maxSub, sum(nums) - minSub)
 
+# 2023/01/18
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        maxSub = float('-inf')
+        curSum = 0
+        for i in range(n):
+            curSum += nums[i]
+            maxSub = max(maxSub, curSum)
+            if curSum < 0:
+                curSum = 0
+
+        minSub = float('inf')
+        curSum = 0
+        temp_l = l = r = 0
+        for i in range(n):
+            curSum += nums[i]
+            if curSum < minSub:
+                l = temp_l
+                r = i
+                minSub = curSum
+            if curSum > 0:
+                temp_l = i
+                curSum = 0
+
+        return max(maxSub, (sum(nums) - minSub) if r-l+1 < n else float('-inf'))
