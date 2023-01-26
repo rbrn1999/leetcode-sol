8ffe65b36d97e757714309a5cb3070ac7c500163
Ian Liu <ianliu@livemail.tw>
2023-01-26 21:51:29 +0800

    add more optimal solution

--- problems/787. Cheapest Flights Within K Stops.py
+++ problems/787. Cheapest Flights Within K Stops.py
@@ -1,8 +1,35 @@
 # link: https://leetcode.com/problems/cheapest-flights-within-k-stops/
 
 from collections import deque, defaultdict
+
+# 2023/01/26: more optimal
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(self, n: int, flights: list[list[int]], src: int, dst: int, k: int) -> int:
+        minPrice = {src: 0}
+        destinationPrice = defaultdict(list)
+        for start, end, price in flights:
+            if start != dst:
+                destinationPrice[start].append([end, price])
+        
+        queue = deque([(src, 0)])
+        steps = 0
+        while queue and steps <= k:
+            for _ in range(len(queue)):
+                node, price = queue.popleft()
+                neighborNodes = set()
+                for neighbor, pathPrice in destinationPrice[node]:
+                    newPrice = price + pathPrice
+                    if newPrice < minPrice.get(neighbor, float('inf')):
+                        minPrice[neighbor] = newPrice
+                        neighborNodes.add(neighbor)
+                queue.extend([(node, minPrice[node]) for node in neighborNodes])
+            steps += 1
+            
+        return minPrice.get(dst, -1)
+
+# 2022/12/31
+class Solution:
+    def findCheapestPrice(self, n: int, flights: list[list[int]], src: int, dst: int, k: int) -> int:
         edges = defaultdict(list)
         for start, end, price in flights:
             if start != dst:
