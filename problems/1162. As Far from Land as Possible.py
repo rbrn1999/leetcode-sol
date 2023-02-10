f0b5df649e786e526e33ba25e4a15bed809c226c
Ian Liu <ianliu@livemail.tw>
2023-02-10 10:50:45 +0800

    refactor

--- problems/1162. As Far from Land as Possible.py
+++ problems/1162. As Far from Land as Possible.py
@@ -5,8 +5,7 @@ from collections import deque
 class Solution:
     def maxDistance(self, grid: list[list[int]]) -> int:
         n = len(grid)
-        distance = 0
-        max_d = -1
+        distance = -1
         q = deque([(row, col) for col in range(n) for row in range(n)if grid[row][col] == 1])
         visited = set()
         # backtrack from every island to water with breadth first search
@@ -17,9 +16,9 @@ class Solution:
                 for d_r, d_c in dirs:
                     nR, nC = row + d_r, col + d_c
                     if 0 <= nR < n and 0 <= nC < n and (nR, nC) not in visited and grid[nR][nC] == 0:
-                        max_d = distance + 1
                         visited.add((nR, nC))
                         q.append((nR, nC))
+
             distance += 1
 
-        return max_d 
+        return distance if distance != 0 else -1
