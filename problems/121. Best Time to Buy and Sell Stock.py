503507a6b70b2b62476f3687f9d154ab5f50cd02
Ian Liu <ianliu@livemail.tw>
2023-02-25 11:01:15 +0800

    refactor solution

--- problems/121. Best Time to Buy and Sell Stock.py
+++ problems/121. Best Time to Buy and Sell Stock.py
@@ -1,16 +1,13 @@
 # link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         low = float('inf')
-        high = -float('inf')
         profit = 0
-        
         for price in prices:
-            if price < low:
-                low = high = price
-            elif price > high:
-                high = price
-                profit = max(profit, high-low)
-                
+            if price > low:
+                profit = max(profit, price-low)
+            else:
+                low = price
+        
         return profit
