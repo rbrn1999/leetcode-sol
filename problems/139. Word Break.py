1211c25ce2554da582e53226d37f33dbaf3fd80f
Ian Liu <ianliu@livemail.tw>
2023-05-02 15:19:38 +0800

    Add bottom-up solution

--- problems/139. Word Break.py
+++ problems/139. Word Break.py
@@ -1,3 +1,4 @@
+# top-bottom, recursion
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
         word_dict = {} # count: [word]
@@ -23,5 +24,52 @@ class Solution:
             return False
 
         return isValid(0)
-
-print(Solution().wordBreak("applepenapple", ["apple","pen"]))
+# bottom-up, don't use length_words to get better space complexity and same time complexity(but slower runtime)
+from collections import defaultdict
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        n = len(s)
+        dp = [True] + [False] * n
+        length_words = defaultdict(set)
+        for word in wordDict:
+            length_words[len(word)].add(word)
+        
+        for i in range(n):
+            if not dp[i]:
+                continue
+            for length in length_words:
+                subString = s[i:i+length]
+                if i+length >= n+1 or dp[i+length]:
+                    continue
+                for word in length_words[length]:
+                    if word == subString:
+                        dp[i+length] = True
+                        break
+        
+        return dp[-1]
+# bottom-up, optimized space, don't use length_words to get better space complexity and same time complexity(but slower runtime)
+from collections import defaultdict
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        m = max(len(word) for word in wordDict)
+        n = len(s)
+        dp = [True] + [False] * m
+        length_words = defaultdict(set)
+        for word in wordDict:
+            length_words[len(word)].add(word)
+        
+        for i in range(n):
+            if not dp[i%(m+1)]:
+                continue
+            for length in length_words:
+                subString = s[i:i+length]
+                if i+length >= n+1 or dp[(i+length)%(m+1)]:
+                    continue
+                for word in length_words[length]:
+                    if word == subString:
+                        dp[(i+length)%(m+1)] = True
+                        break
+            if i < n-1:
+                dp[i%(m+1)] = False
+            
+        return dp[n%(m+1)]
