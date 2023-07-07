d9a149db086519bb19af289e08ea184f845f7cdb
Ian Liu <ianliu@livemail.tw>
2023-07-08 00:51:50 +0800

    update solution

--- problems/2448. Minimum Cost to Make Array Equal.py
+++ problems/2448. Minimum Cost to Make Array Equal.py
@@ -1,14 +1,31 @@
-# Time Limit Exceeded
+# link: https://leetcode.com/problems/minimum-cost-to-make-array-equal/
+
 class Solution:
     def minCost(self, nums: list[int], cost: list[int]) -> int:
-        res = float('inf')
-        for i, target in enumerate(nums):
-            curCost = 0
-            for j in range(len(nums)):
-                if i == j:
-                    continue
-                curCost += abs(nums[j]-target)*cost[j]
-                if curCost >= res:
-                    break
-            res = min(res, curCost)
-        return res
+        num_cost = sorted(zip(nums, cost))
+        curSum = 0
+        preSum = []
+        for num, _ in num_cost:
+            curSum += num
+            preSum.append(curSum)
+
+
+        # initailize the base cost 
+        result = 0
+        for num, curCost in num_cost:
+            result += curCost * (num-num_cost[0][0])
+
+        curCost = result
+        suffixCost = sum(cost) - num_cost[0][1]
+        prefixCost = num_cost[0][1]
+
+        for i in range(1, len(num_cost)):
+            num, cost = num_cost[i]
+            diff = num - num_cost[i-1][0]
+            curCost += diff * prefixCost - diff * suffixCost
+            result = min(result, curCost)
+            prefixCost += cost
+            suffixCost -= cost
+        
+        return result
+            
