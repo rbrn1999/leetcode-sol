9d831428169c4b13fdb3fc498d842dbdf9920f3e
Ian Liu <ianliu@livemail.tw>
2023-08-08 19:36:50 +0800

    change to iterative binary search

--- problems/33. Search in Rotated Sorted Array.py
+++ problems/33. Search in Rotated Sorted Array.py
@@ -3,23 +3,24 @@
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        def bsearch(low, high):
-            if low > high:
-                return -1
-            
-            mid = low + (high-low)//2
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
 
-            if nums[mid] >= nums[low]: # mid at left portion
-                if target > nums[mid] or target < nums[low]: # (larger than mid) or (less than mid and smaller than low)
-                    return bsearch(mid+1, high)
-                else: #(less than mid and larger than low)
-                    return bsearch(low, mid-1)
-            else:                      # mid at right portion
-                if target < nums[mid] or target > nums[high]: # (less than mid) or (larger than mid and larger than high)
-                    return bsearch(low, mid-1)
-                else: # (larger than mid and lower than high)
-                    return bsearch(mid+1, high)
-
-        return bsearch(0, len(nums)-1)
+            if nums[mid] >= nums[left]:
+                # left portion of the array
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                # right portion of the array
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        
+        return -1
