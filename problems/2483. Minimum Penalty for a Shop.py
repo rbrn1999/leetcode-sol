d82d17761056d627c8bd8a574100bab26f4f0c97
Ian Liu <ianliu@livemail.tw>
2023-08-29 20:22:46 +0800

    add more solutions

--- problems/2483. Minimum Penalty for a Shop.py
+++ problems/2483. Minimum Penalty for a Shop.py
@@ -1,17 +1,58 @@
 # link: https://leetcode.com/problems/minimum-penalty-for-a-shop/
 
+# 2-pass straight forward
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n = len(customers)
-        cur = customers.count('Y')
-        res = (0, cur)
+        Y_right = customers.count('Y')
+        N_left = 0
+        min_penalty = Y_right
+        answer = 0
         for i in range(1, n+1):
-            if customers[i-1] == 'Y':
-                cur -= 1
-                if cur < res[1]:
-                    res = (i, cur)
+            N_left += int(customers[i-1] == 'N')
+            Y_right -= int(customers[i-1] == 'Y')
+            penalty = N_left + Y_right
+            if penalty < min_penalty:
+                min_penalty = penalty
+                answer = i
+        
+        return answer
+    
+# alternative solution
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+	    # default: closed at hour 0
+        answer = 0
+        min_penalty = penalty = customers.count('Y')
+        for i in range(n):
+			# closed at i+1: add customer[i] to the "left"
+            if customers[i] == 'Y':
+                penalty -= 1
             else:
-                cur += 1
+                penalty += 1
+            if penalty < min_penalty:
+                min_penalty = penalty
+                answer = i+1
+        
+        return answer
 
-        return res[0]
+# one pass: only care relative value
+
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+        penalty = min_penalty = 0
+        answer = 0
+        for i in range(n):
+            if customers[i] == 'Y':
+                penalty -= 1
+            else:
+                penalty += 1
+            if penalty < min_penalty:
+                min_penalty = penalty
+                answer = i+1
+        
+        return answer
+
 
