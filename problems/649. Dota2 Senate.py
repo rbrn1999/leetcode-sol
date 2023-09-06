c7f1b2aa9349841c98b22e0ccdb97391de563cda
Ian Liu <ianliu@livemail.tw>
2023-09-06 17:39:02 +0800

    change to one queue solution

--- problems/649. Dota2 Senate.py
+++ problems/649. Dota2 Senate.py
@@ -1,38 +1,30 @@
+# link: https://leetcode.com/problems/dota2-senate
+
 from collections import deque
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        Dr = deque()
-        Rr = deque()
-        Dl = deque()
-        Rl = deque()
-        for i, c in enumerate(senate):
-            if c == 'D':
-                Dr.append(i)
-            else:
-                Rr.append(i)
-        while Dr or Rr:
-            if Dr and (not Rr or Dr[0] < Rr[0]):
-                Dl.append(Dr.popleft())
-                if Rr:
-                    Rr.popleft()
-                elif Rl:
-                    Rl.popleft()
+        r = senate.count('R')
+        d = len(senate) - r
+
+        ban_r = 0
+        ban_d = 0
+        q = deque(senate)
+        while r > 0 and d > 0:
+            for _ in range(len(q)):
+                c = q.popleft()
+                if c == 'R':
+                    if ban_r > 0:
+                        ban_r -= 1
+                        r -= 1
+                    else:
+                        ban_d += 1
+                        q.append(c)
                 else:
-                    return "Dire"
-            else:
-                Rl.append(Rr.popleft())
-                if Dr:
-                    Dr.popleft()
-                elif Dl:
-                    Dl.popleft()
-                else:
-                    return "Radiant"
-            if not Dr and not Rr:
-                if Dl and not Rl:
-                    return "Dire"
-                if Rl and not Dl:
-                    return "Radiant"
-                Dl, Dr = deque(), Dl
-                Rl, Rr = deque(), Rl
+                    if ban_d > 0:
+                        ban_d -= 1
+                        d -= 1
+                    else:
+                        ban_r += 1
+                        q.append(c)
         
-        return "ERROR"
+        return "Dire" if d > 0 else "Radiant"
