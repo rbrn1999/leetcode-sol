c24892fbc7577364cce222c80815d44d63c3bb58
Ian Liu <ianliu@livemail.tw>
2023-09-11 14:06:05 +0800

    add more solutions

--- problems/1143. Longest Common Subsequence.py
+++ problems/1143. Longest Common Subsequence.py
@@ -48,3 +48,34 @@ class Solution:
             return length
 
         return dfs(0, 0)
+
+# Bottom-Up iterative
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0]*(n+1) for _ in range(m+1)] # dp[i][j]: max subsequence of text1[:i], text2[:j]
+        for i in range(m):
+            for j in range(n):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1
+                else:
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+        
+        return dp[m][n]
+    
+# Bottom-Up optimized space
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if len(text2) > len(text1):
+            text1, text2 = text2, text1
+        m, n = len(text1), len(text2)
+        dp = [[0]*(n+1) for _ in range(2)]
+        for i in range(m):
+            for j in range(n):
+                if text1[i] == text2[j]:
+                    dp[1][j+1] = dp[0][j] + 1
+                else:
+                    dp[1][j+1] = max(dp[1][j], dp[0][j+1])
+            dp = [dp[1], [0]*(n+1)]
+        
+        return dp[0][n]
