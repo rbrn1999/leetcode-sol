135eeb130bdec2d65e8cd8e3deb24e6b39d07c8a
Ian Liu <ianliu@livemail.tw>
2023-10-23 10:04:39 +0800

    add loop solution

--- problems/342. Power of Four.py
+++ problems/342. Power of Four.py
@@ -3,3 +3,12 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n != 0 and n&(n-1) == 0 and n&1431655765 == n
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        while n > 1:
+            if n % 4:
+                return False
+            n //= 4
+
+        return True if n > 0 else False
