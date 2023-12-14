d73a0dcc2df558dac80bce4f1bb65f5ceeb48255
Ian Liu <ianliu@livemail.tw>
2023-12-14 13:05:26 +0800

    update solution

--- problems/2482. Difference Between Ones and Zeros in Row and Column.py
+++ problems/2482. Difference Between Ones and Zeros in Row and Column.py
@@ -1,14 +1,16 @@
 # link: https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column/
 
 class Solution:
-    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+    def onesMinusZeros(self, grid: list[list[int]]) -> list[list[int]]:
         m, n = len(grid), len(grid[0])
-        diff = [[float('inf')] * n for _ in range(m)]
-        rows = [sum(grid[i]) for i in range(m)]
-        cols = [sum([grid[r][c] for r in range(m)]) for c in range(n)]
-
-        for r in range(m):
-            for c in range(n):
-                diff[r][c] = 2 * (rows[r] + cols[c]) - m - n
-
+        diff = [[0] * n for _ in range(m)]
+        for row in range(m):
+            val = sum(grid[row][col] if grid[row][col] == 1 else -1 for col in range(n))
+            for col in range(n):
+                diff[row][col] += val
+        for col in range(n):
+            val = sum(grid[row][col] if grid[row][col] == 1 else -1 for row in range(m))
+            for row in range(m):
+                diff[row][col] += val
+        
         return diff
