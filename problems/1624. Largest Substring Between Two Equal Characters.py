4d5dc800f9117c2d0ae822da3eb340dab95978d2
Ian Liu <ianliu@livemail.tw>
2024-01-01 10:54:14 +0800

    add hashmap approach

--- problems/1624. Largest Substring Between Two Equal Characters.py
+++ problems/1624. Largest Substring Between Two Equal Characters.py
@@ -1,5 +1,6 @@
 # link: https://leetcode.com/problems/largest-substring-between-two-equal-characters/
 
+# Array
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
         low_high = [[-1, -1] for _ in range(26)]
@@ -11,3 +12,21 @@ class Solution:
                 low_high[ord(s[i]) - ord('a')][1] = i
 
         return max(high-low-1 for low, high in low_high)
+
+# Hash Map
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        first = {}
+        last = {}
+        for i in range(len(s)):
+            if s[i] not in first:
+                first[s[i]] = i
+            else:
+                last[s[i]] = i
+        
+        result = -1
+        for c in first:
+            if c in last:
+                result = max(result, last[c] - first[c] - 1)
+        
+        return result
