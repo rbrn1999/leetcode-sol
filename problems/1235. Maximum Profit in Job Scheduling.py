7814b8532b5dcd87547efa618f4a049e9b59e597
Ian Liu <ianliu@livemail.tw>
2024-01-07 07:24:11 +0800

    add top-down dp solution

--- problems/1235. Maximum Profit in Job Scheduling.py
+++ problems/1235. Maximum Profit in Job Scheduling.py
@@ -1,7 +1,31 @@
 # link: https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+
+# Top-Down DP
+from functools import cache
+import bisect
+class Solution:
+    def jobScheduling(self, startTime: list[int], endTime: list[int], profit: list[int]) -> int:
+        jobs = sorted(zip(startTime, endTime, profit))
+
+        @cache
+        def helper(i: int) -> int:
+            if i == len(jobs):
+                return 0
+
+            # skip this job
+            p = helper(i+1)
+
+            # take this job
+            j = bisect.bisect(jobs, (jobs[i][1], -1, -1))
+            p = max(p, jobs[i][2] + helper(j))
+
+            return p
+        
+        return helper(0)
+
 # solution reference: https://leetcode.com/problems/maximum-profit-in-job-scheduling/discuss/409009/
 class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+    def jobScheduling(self, startTime: list[int], endTime: list[int], profit: list[int]) -> int:
         jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
         memo = [[0, 0]]
         for start, end, profit in jobs:
