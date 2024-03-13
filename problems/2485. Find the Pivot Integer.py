92382e7ee0865692a9b6d755466ebe4cfb85627a
Ian Liu <ianliu@livemail.tw>
2024-03-13 09:17:12 +0800

    add binary search solution

--- problems/2485. Find the Pivot Integer.py
+++ problems/2485. Find the Pivot Integer.py
@@ -11,3 +11,21 @@ class Solution:
             left -= num
 
         return -1
+
+# Binary Search
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        left = 1
+        right = n
+        while left <= right:
+            mid = (left + right) // 2
+            leftSum = (1 + mid) * mid // 2
+            rightSum = (mid + n) * (n - mid + 1) // 2
+            if leftSum < rightSum:
+                left = mid + 1
+            elif leftSum > rightSum:
+                right = mid - 1
+            else:
+                return mid
+        
+        return -1
