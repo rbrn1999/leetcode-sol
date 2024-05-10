8a605edbec60e87c277b7ebcf952049244de2710
Ian Liu <ianliu@livemail.tw>
2024-05-10 13:04:57 +0800

    update solution

--- problems/2487. Remove Nodes From Linked List.py
+++ problems/2487. Remove Nodes From Linked List.py
@@ -1,33 +1,44 @@
 # link: https://leetcode.com/problems/remove-nodes-from-linked-list/
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        max_list = collections.deque()
+        # reverse list
+        prev = None
         cur = head
         while cur:
-            max_list.append(cur.val)
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        head = prev
+        # traverse in reversed order, keep track of current max
+        # if the current max is larger than the node, remove it
+        curMax = 0
+        prev = ListNode()
+        cur = head
+        while cur:
+            curMax = max(curMax, cur.val)
+            if cur.val < curMax:
+                prev.next = cur.next
+            else:
+                prev = cur
+
             cur = cur.next
 
-        for i in range(len(max_list)-2, -1, -1):
-            max_list[i] = max(max_list[i], max_list[i+1])
-
+        # reverse the link back to the original order
         cur = head
         prev = None
-        while cur:
-            if max_list.popleft() > cur.val:
-                if cur is head:
-                    cur = cur.next
-                    head = cur
-                else:
-                    prev.next = cur.next
-                    cur = cur.next
-            else:
-                prev, cur = cur, cur.next
 
-        return head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        return prev
