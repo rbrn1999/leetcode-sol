db46c18640a9b3a6d125019c12aae3b85d3b209c
Ian Liu <ianliu@livemail.tw>
2024-06-08 15:43:42 +0800

    update solution

--- problems/523. Continuous Subarray Sum.py
+++ problems/523. Continuous Subarray Sum.py
@@ -1,21 +1,20 @@
 # link: https://leetcode.com/problems/continuous-subarray-sum/
 
 class Solution:
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
-        accu = [0]
-        for i in range(n):
-            accu.append(accu[-1] + nums[i])
-        '''
-        acc[start]%k == acc[end]%k implies (acc[end] - acc[start]) % k == 0
-        ->
-        find acc[start]%k == acc[end]%k, where start + 2 <= end
-        '''
-        mod = {}
-        for i, val in enumerate(accu):
-            if val % k in mod and i - mod[val % k] >= 2:
-                return True
-            else:
-                mod.setdefault(val % k, i)
+    def checkSubarraySum(self, nums: list[int], k: int) -> bool:
+        prefix_sum_index = {} # store sum(0...i) % k
+        prev = 0
+        prefix_sum_index[0] = 0
+
+        for i, num in enumerate(nums):
+            cur_sum = (prev + num) % k
+            if cur_sum in prefix_sum_index:
+                if i - prefix_sum_index[cur_sum] + 1 >= 2:
+                    return True
+                else:
+                    continue
+
+            prefix_sum_index[cur_sum] = i + 1
+            prev = cur_sum
 
         return False
