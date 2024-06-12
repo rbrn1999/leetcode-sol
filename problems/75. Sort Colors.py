4818238f6f8f065edbb60f611700ed3f9ef16f95
Ian Liu <ianliu@livemail.tw>
2024-06-12 09:27:06 +0800

    update solution

--- problems/75. Sort Colors.py
+++ problems/75. Sort Colors.py
@@ -1,15 +1,20 @@
 # link: https://leetcode.com/problems/sort-colors/
 
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        colorCount = [0] * 3
-        for num in nums:
-            colorCount[num] += 1
-        i = 0
-        for color in range(len(colorCount)):
-            nums[i:i+colorCount[color]] = [color] * colorCount[color]
-            i += colorCount[color]
-
+        l = 0
+        m = 0
+        r = len(nums) - 1
+        while m <= r:
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m = max(l, m)
+            elif nums[m] == 1:
+                m += 1
+            else:
+                nums[r], nums[m] = nums[m], nums[r]
+                r -= 1
