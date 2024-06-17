0856096b2c66ba41b0772be2be57f58df08830ed
Ian Liu <ianliu@livemail.tw>
2024-06-17 08:10:03 +0800

    update solution

--- problems/633. Sum of Square Numbers.py
+++ problems/633. Sum of Square Numbers.py
@@ -1,17 +1,11 @@
 # link: https://leetcode.com/problems/sum-of-square-numbers/
 
+import math
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        sqrt_c = int(math.sqrt(c))
-        if sqrt_c ** 2 == c:
-            return True
-
-        nums = [num ** 2 for num in range(1, int(math.sqrt(c))+1)]
-        s = set()
-        for num in nums:
-            s.add(c - num)
-            if num in s:
+        for a in range(int(math.sqrt(c))+1):
+            b_square = c - a ** 2
+            if b_square == int(math.sqrt(b_square)) ** 2:
                 return True
 
         return False
-
