725ee1afd71320edec4c7954aeefaa828107e430
Ian Liu <ianliu@livemail.tw>
2024-06-23 10:55:22 +0800

    add solution

--- problems/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ problems/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -1,5 +1,35 @@
 # link: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
 
+# 2 Deques
+
+from collections import deque
+class Solution:
+    def longestSubarray(self, nums: list[int], limit: int) -> int:
+        max_dq = deque() # decreasing order
+        min_dq = deque() # increasing order
+        max_length = 0
+
+        l = 0
+        for r, num in enumerate(nums):
+            while max_dq and max_dq[-1] < num:
+                max_dq.pop()
+            max_dq.append(num)
+
+            while min_dq and min_dq[-1] > num:
+                min_dq.pop()
+            min_dq.append(num)
+            
+            while max_dq[0] - min_dq[0] > limit:
+                if max_dq[0] == nums[l]:
+                    max_dq.popleft()
+                if min_dq[0] == nums[l]:
+                    min_dq.popleft()
+                l += 1
+
+            max_length = max(max_length, r-l+1)
+        
+        return max_length
+
 # SortedList (tree set, multi set)
 from sortedcontainers import SortedList
 class Solution:
