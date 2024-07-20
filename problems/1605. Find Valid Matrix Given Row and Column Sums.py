39c2ea7c8ecbf76d4bf62c82fdc4181a55f74b90
Ian Liu <ianliu@livemail.tw>
2024-07-20 14:06:20 +0800

    add optimal solution for problem 1605

--- problems/1605. Find Valid Matrix Given Row and Column Sums.py
+++ problems/1605. Find Valid Matrix Given Row and Column Sums.py
@@ -1,5 +1,33 @@
 # link: https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
 
+# Greedy (reference: https://youtu.be/Ks6fGnXkHPg)
+class Solution:
+    def restoreMatrix(self, rowSum: list[int], colSum: list[int]) -> list[list[int]]:
+        # fill each cell with the maximum number possible, then shift the extra value to the next cell
+
+        m, n = len(rowSum), len(colSum)
+        matrix = [[0] * n for _ in range(m)]
+
+        for row in range(m):
+            matrix[row][0] = rowSum[row]
+        
+        for col in range(n):
+            cur_col_sum = sum(matrix[row][col] for row in range(m))
+
+            row = 0
+            while cur_col_sum > colSum[col]:
+                diff = cur_col_sum - colSum[col]
+                max_shift = min(matrix[row][col], diff)
+
+                matrix[row][col] -= max_shift
+                matrix[row][col+1] += max_shift
+                cur_col_sum -= max_shift
+                row += 1
+        
+        return matrix
+        
+
+# apporach with hint
 class Solution:
     def restoreMatrix(self, rowSum: list[int], colSum: list[int]) -> list[list[int]]:
         m, n = len(rowSum), len(colSum)
