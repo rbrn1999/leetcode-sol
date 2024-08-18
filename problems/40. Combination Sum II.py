27df1ff71930e289f5cf5eedf039e3938212fd5d
Ian Liu <ianliu@livemail.tw>
2024-08-18 18:09:34 +0800

    add new solution

--- problems/40. Combination Sum II.py
+++ problems/40. Combination Sum II.py
@@ -1,8 +1,38 @@
 # link: https://leetcode.com/problems/combination-sum-ii/
-# solution reference: https://leetcode.com/problems/combination-sum-ii/solutions/16861/
 
+# Counting
+from collections import Counter
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
+        num_count = Counter(candidates)
+        nums = list(num_count.keys())
+        result = []
+
+        def dfs(i: int, chosen: list[int], curSum: int) -> None:
+            if curSum == target:
+                result.append(chosen.copy())
+                return
+            if curSum > target:
+                return
+            if i == len(nums):
+                return
+
+            for j in range(i, len(nums)):
+                for count in range(1, num_count[nums[j]]+1):
+                    chosen.append(nums[j])
+                    curSum += nums[j]
+                    dfs(j+1, chosen, curSum)
+                curSum -= nums[j] * num_count[nums[j]]
+                del chosen[-num_count[nums[j]]:]
+
+        dfs(0, [], 0)
+        return result
+
+
+# Sorting
+# solution reference: https://leetcode.com/problems/combination-sum-ii/solutions/16861/
+class Solution:
+    def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
         n = len(candidates)
         candidates.sort()
         result = []
