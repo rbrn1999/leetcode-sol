bdd1d5ce4153e615446ea0e10c8c9b7f3c1a013b
rbrn1999 <41038139+rbrn1999@users.noreply.github.com>
2024-10-02 12:36:15 +0800

    add problem 1331

---
+++ problems/1331. Rank Transform of an Array.py
@@ -0,0 +1,6 @@
+# link: https://leetcode.com/problems/rank-transform-of-an-array/
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {num: i+1 for i, num in  enumerate(sorted(set(arr)))}
+        return [rank[num] for num in arr]
